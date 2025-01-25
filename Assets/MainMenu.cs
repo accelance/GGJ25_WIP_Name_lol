9d340bb5409d8a1b2e0a75ec402c8ae22fadd9fe
Jiaying He <he.jiaying@student.hs-anhalt.de>
2025-01-25 22:36:26 +0100

    play btn

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour,IPointerDownHandler, IPointerUpHandler
 {
+    public Image _image;
+    public Sprite _default, _pressed;
+    public AudioClip _compressClip, _uncompressClip;
+    public AudioSource _source;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    public void PlayGame() {
-    SceneManager.LoadSceneAsync(1);
+    SceneManager.LoadSceneAsync("Game");
    }
 
    public void QuitGame() {
     Application.Quit();
+   }
+
+   public void OnPointerDown(PointerEventData eventData){
+    _image.sprite = _pressed;
+    _source.PlayOneShot(_compressClip);
+    
+   }
+
+   public void OnPointerUp(PointerEventData eventData){
+    _image.sprite = _default;
+    _source.PlayOneShot(_uncompressClip);
    }
 }

3836adce7d84a83239ddd3fdcdc35bb46c869db9
Jiaying He <he.jiaying@student.hs-anhalt.de>
2025-01-26 14:50:18 +0100

    UI mainmenu and pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject PauseButton;
+    public GameObject PausePanel;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            if (gameIsPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Continue();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        PauseButton.SetActive(true);
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
